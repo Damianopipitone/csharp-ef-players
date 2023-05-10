2740c13619b59ec253f85170e7cd70d9b7fcf298
Damianopipitone <damianopipitone33@gmail.com>
2023-05-10 17:32:59 +0200

    fatto una migration

--- C# Entity Framework Players/Program.cs
+++ C# Entity Framework Players/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("");
+﻿using C__Entity_Framework_Players;
+using System;
+
+using (PlayerContext db  = new PlayerContext())
+{
+    // CREATE
+    Random random = new Random();
+    int scoreNumber = random.Next(1, 10);
+    int gamePlayedNumber = random.Next(1, 100);
+    int gameWonNumber = random.Next(1, gamePlayedNumber +1);
+
+    Player newPlayer = new Player { Name = "Cristiano", Id = "AB567", Surname = "Ronaldo", Score = scoreNumber, GameNumberPlayed = gamePlayedNumber, GameWon = gameWonNumber };
+
+    db.Add(newPlayer);
+    db.SaveChanges();
+
+    List<Player> player = db.Player.OrderBy(player => player.Name).ToList<Player>();
+
+}
