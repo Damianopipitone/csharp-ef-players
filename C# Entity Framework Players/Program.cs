49d119fecb64302d52a75a99a85ce45d836fc86f
Damianopipitone <damianopipitone33@gmail.com>
2023-05-12 15:15:43 +0200

    Eccezioni

--- C# Entity Framework Players/Program.cs
+++ C# Entity Framework Players/Program.cs
@@ -3,17 +3,25 @@ using System;
 
 using (PlayerContext db  = new PlayerContext())
 {
-    // CREATE
-    Random random = new Random();
-    int scoreNumber = random.Next(1, 10);
-    int gamePlayedNumber = random.Next(1, 100);
-    int gameWonNumber = random.Next(1, gamePlayedNumber +1);
+    try
+    {
+        // CREATE
+        Random random = new Random();
+        int scoreNumber = random.Next(1, 10);
+        int gamePlayedNumber = random.Next(1, 100);
+        int gameWonNumber = random.Next(1, gamePlayedNumber +1);
 
-    Player newPlayer = new Player("Cristiano", "Ronaldo", "AGDGG56", scoreNumber, gamePlayedNumber, gameWonNumber);
+         Player newPlayer = new Player("Cristiano", "Ronaldo", "AGDGG56", scoreNumber, gamePlayedNumber, gameWonNumber);
 
-    db.Add(newPlayer);
-    db.SaveChanges();
+        db.Add(newPlayer);
+        db.SaveChanges();
+        Console.WriteLine(newPlayer.ToString());
 
-    List<Player> player = db.Player.OrderBy(player => player.Name).ToList<Player>();
+        List<Player> player = db.Player.OrderBy(player => player.Name).ToList<Player>();
+    } catch (Exception ex)
+    {
+        Console.WriteLine("Qualcosa è andato storto");
+    }
+    
 
 }
